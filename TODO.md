c1af581a9d3101d7bedb0bdcd19acd06ed61ea94
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-03-05 12:36:37 +0100

    Création du projet Maven

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+- [ ] Réimplémenter le moteur de calcul
+- [ ] Réimplémenter le téléchargement des données
+- [ ] Réimplémenter l'interface CLI
---
+++ src/main/java/fr/jordi_rocafort/tle_decoder/App.java
@@ -0,0 +1,13 @@
+package fr.jordi_rocafort.tle_decoder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
