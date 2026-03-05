d3c2f3639ac438dcd99e297c510ca8bb748af373
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-03-05 13:38:31 +0100

    Traduction de Coords2D et Coords3D

--- src/main/java/fr/jordi_rocafort/tle_decoder/model/data/Coords2D.java
+++ src/main/java/fr/jordi_rocafort/tle_decoder/model/data/Coords2D.java
@@ -1,5 +1,7 @@
 package fr.jordi_rocafort.tle_decoder.model.data;
 
-public record Coords2D() {
-
+public record Coords2D(double x, double y) {
+	public double getDistanceToOrigin() {
+		return Math.sqrt(x * x + y * y);
+	}
 }
